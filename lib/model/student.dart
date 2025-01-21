d9b6c73fa7ebb222ea5c2035dfa0539e2c3ff42b
abiinavvv <abhinavaneesh2@gmail.com>
2025-01-21 16:06:20 +0530

    GetX'

--- lib/model/student.dart
+++ lib/model/student.dart
@@ -1,31 +1,4 @@
-// class Student {
-//   int? id;
-//   final String name;
-//   final int age;
-//   final String department;
-//   final String email;
-//   final String imageurl;
 
-//   Student({
-//     this.id,
-//     required this.name,
-//     required this.age,
-//     required this.department,
-//     required this.email,
-//     required this.imageurl,
-//   });
-
-//   static fromMap(Map<String, dynamic> std) {
-//     return Student(
-//       id: std['id'],
-//       name: std['name'] ?? '',
-//       age: std['age'] ?? 0,
-//       department: std['department'] ?? '',
-//       email: std['email'] ?? '',
-//       imageurl: std['imageurl'] ?? '',
-//     );
-//   }
-// }
 class Student {
   //FIELDS AS ATTRIBUTES OF CLASS
   int? id;
